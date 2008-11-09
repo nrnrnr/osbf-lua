6da77fa37fbc06ea3b5924bfff11d1b62ad31051
fidelis <fidelis>
2008-11-09 03:57:17 +0000

    - removal of .in - first part   - no more .c.in   - no more .lua.in in lua dir - commented out target mem-test. - make distcheck now works!
     TODO: #! lines, examples and testing (more?)

--- main.c
+++ main.c
@@ -2,15 +2,16 @@
 
 #include "lauxlib.h"
 #include "lualib.h"
+#include "osbf-modname.h"
 
 extern int lua_interp(int argc, char **argv, void (*)(lua_State*));
-extern int luaopen_osbf3_core(lua_State *);
+extern int OPENFUN(lua_State *);
 extern int luaopen_fastmime(lua_State *);
 
 static void open_osbf(lua_State *L) {
   int top = lua_gettop(L);
-  lua_pushstring(L, "osbf3.core");
-  (void)luaopen_osbf3_core(L);
+  lua_pushstring(L, QUOTE(OSBF_MODNAME)".core");
+  (void)OPENFUN(L);
   lua_pushstring(L, "fastmime");
   (void)luaopen_fastmime(L);
   lua_settop(L, top);
