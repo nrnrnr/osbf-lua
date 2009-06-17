cd550397ad0bb01ec3cd008122d64569463c02f7
Norman Ramsey <nr@cs.tufts.edu>
2009-06-17 13:24:14 -0400

    Changed testing/modname.lua to get the module name by running 'mk' instead of grubbing through a .h file that no longer exists.

--- testing/modname.lua
+++ testing/modname.lua
@@ -1,9 +1,29 @@
-local f = assert(io.open '../osbf-modname.h') -- very bogus
-for l in f:lines() do
-  local modname = l:match('^%#define%s+OSBF_MODNAME%s+(%S+)%s*$')
-  if modname then
-    f:close()
-    return modname
-  end
+assert(arg[0]) -- only works in a script
+
+local function dirname(s)
+  s = s:gsub('/$', '')
+  local s, n = s:gsub('/[^/]*$', '')
+  if n == 1 then return s else return '.' end
 end
-error("Cannot find module name in header file")
+
+local mkfiledir = dirname(arg[0]) .. '/../handbuild/'
+
+local function capture(cmd, raw)
+  local f = assert(io.popen(cmd, 'r'))
+  local s = assert(f:read('*a'))
+  f:close()
+  if raw then return s end
+  s = string.gsub(s, '^%s+', '')
+  s = string.gsub(s, '%s+$', '')
+  s = string.gsub(s, '[\n\r]+', ' ')
+  return s
+end
+
+-- probably needs a fix that uses Make not mk, but will do for now...
+
+local modname = capture(string.format("(cd '%s' && mk modname)", mkfiledir))
+if modname:find '%a' then
+  return modname
+else
+  error("Cannot get module name out of mkfile")
+end
